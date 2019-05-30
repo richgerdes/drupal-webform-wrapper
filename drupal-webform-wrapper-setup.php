93a007661e6b5165a3822602fb4cf8c81dc298cc
Rich Gerdes <reg98@oit.rutgers.edu>
2019-05-30 10:50:20 -0400

    Add logic to copy installer path config.

--- drupal-webform-wrapper-setup.php
+++ drupal-webform-wrapper-setup.php
@@ -24,15 +24,15 @@ function has_package_repository($local_repo, $name) {
             if ($repository['type'] === 'package') {
                 if (isset($repository['package']['name'])) {
                     if ($repository['package']['name'] === $name) {
-                        # Package definition exists.
+                        // Package definition exists.
                         return $key;
                     }
                 }
                 elseif (is_array($repository['package'])) {
-                    # Multiple versions are defined for the package.
+                    // Multiple versions are defined for the package.
                     $package_names = array_column($repository['package'], 'name');
                     if (in_array($name, $package_names)) {
-                        # Defined versions are for the target package.
+                        // Defined versions are for the target package.
                         return $key;
                     }
                 }
@@ -42,10 +42,14 @@ function has_package_repository($local_repo, $name) {
     return false;
 }
 
-$upstream_composer_config_file = file_get_contents($COMPOSER_JSON_URL);
+$curl = curl_init($COMPOSER_JSON_URL);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+$upstream_composer_config_file = curl_exec($curl);
+curl_close($curl);
+
 $upstream_composer_config = json_decode($upstream_composer_config_file, true);
 $local_repo = json_decode(file_get_contents('./composer.json'), true);
-#var_export($upstream_composer_config);
 
 
 foreach ($upstream_composer_config['repositories'] as $repository) {
@@ -60,10 +64,10 @@ foreach ($upstream_composer_config['repositories'] as $repository) {
             $upstream_version = $repository['package']['version'];
             $local_definition = $local_repo['repositories'][$key];
             if (isset($local_definition['package']['name'])) {
-                # Simple package definiton. Verify version.
+                // Simple package definiton. Verify version.
                 $local_version = $local_definition['package']['version'];
                 if ($local_version !== $upstream_version) {
-                    # Upstream has a new version, add it to the list.
+                    // Upstream has a new version, add it to the list.
                     $local_repo['repositories'][$key]['package'] = [
                         $local_definition['package'],
                         $repository['package'],
@@ -71,10 +75,10 @@ foreach ($upstream_composer_config['repositories'] as $repository) {
                 }
             }
             else {
-                # Mutiple defined versions, verify version doesn't exist.
+                // Mutiple defined versions, verify version doesn't exist.
                 $package_versions = array_column($local_definition['package'], 'version');
                 if (!in_array($upstream_version, $package_versions)) {
-                    # Add repo if version not defined.
+                    // Add repo if version not defined.
                     $local_repo['repositories'][$key]['package'][] = $repository['package'];
                 }
             }
@@ -85,4 +89,35 @@ foreach ($upstream_composer_config['repositories'] as $repository) {
     }
 }
 
-file_put_contents('./composer.json', json_encode($local_repo, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL, JSON_PRETTY_PRINT);
+// Set installer paths.
+$upstream_types = $upstream_composer_config['extra']['installer-types'];
+if (isset($local_repo['extra']['installer-types'])) {
+    // Other paths exist. Merge them.
+    $local_types = $local_repo['extra']['installer-types'];
+    $local_repo['extra']['installer-types'] = array_unique(array_merge($local_types, $upstream_types));
+}
+else {
+    // No other paths, use upstream.
+    $local_repo['extra']['installer-types'] = $upstream_types;
+}
+
+if (isset($local_repo['extra']['installer-paths'])) {
+    $local_installers = &$local_repo['extra']['installer-paths'];
+    // Other paths are configured. Merge configuration.
+    foreach ($upstream_composer_config['extra']['installer-paths'] as $installer_path => $packages) {
+        if (isset($local_installers[$installer_path])) {
+            $local_installers[$installer_path] = array_unique(array_merge($packages, $local_installers[$installer_path]));
+        }
+        else {
+            $local_installers[$installer_path] = $packages;
+        }
+    }
+}
+else {
+    // No other configured paths, just add ours.
+    $local_repo['extra']['installer-paths'] = $upstream_composer_config['extra']['installer-paths'];
+}
+
+$json_options = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES;
+$composer_json = json_encode($local_repo, $json_options);
+file_put_contents('./composer.json',  $composer_json . PHP_EOL);
